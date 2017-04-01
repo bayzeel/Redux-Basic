81766767c43e5e7a41ee4f5c0e7fd3c168cb1e8c
Basil Sklepkovich <vsklepkovich@gmail.com>
2017-04-01 19:40:29 +0300

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,12 +2,57 @@ var redux = require("redux");
 
 console.log("Starting redux example");
 
-var reducer = (state = {name: "Anonymous"}, action) => {
+var stateDefault = {
+    name: "Anonymous",
+    hobbies: [],
+    movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
     switch(action.type){
         case "CHANGE_NAME":
             return{
                 ...state,
                 name: action.name
+            };
+        case "ADD_HOBBY":
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };
+        case "REMOVE_HOBBY":
+            return {
+                ...state,
+                hobbies: state.hobbies.filter((hobby) => {
+                    return hobby.id !== action.id;
+                })
+            };
+        case "ADD_MOVIE":
+            return{
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            };
+        case "REMOVE_MOVIE":
+            return {
+                ...state,
+                movies: state.movies.filter((movie) => {
+                    return movie.id !== action.id;
+                })
             };
         default:
             return state;
@@ -24,6 +69,8 @@ var unsubscribe = store.subscribe(() => {
 
     console.log("Name is", state.name);
     document.getElementById("app").innerHTML = state.name;
+
+    console.log("New state", store.getState());
 });
 // unsubscribe();
 
@@ -33,6 +80,38 @@ console.log("currentState", currentState);
 store.dispatch({
     type: "CHANGE_NAME",
     name: "Vasyl"
+});
+
+store.dispatch({
+    type: "ADD_HOBBY",
+    hobby: "Running"
+});
+
+store.dispatch({
+    type: "ADD_HOBBY",
+    hobby: "Walking"
+});
+
+store.dispatch({
+    type: "REMOVE_HOBBY",
+    id: 2
+});
+
+store.dispatch({
+    type: "ADD_MOVIE",
+    title: "Revolver",
+    genre: "criminal drama, action"
+});
+
+store.dispatch({
+    type: "ADD_MOVIE",
+    title: "Prestige",
+    genre: "drama, psychological drama"
+});
+
+store.dispatch({
+    type: "REMOVE_MOVIE",
+    id: 2
 });
 
 store.dispatch({
